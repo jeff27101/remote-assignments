cb9f365599c4f62970bd9c6dd48b830d412d66e0
jeff27101 <jeff27101@gmail.com>
2021-08-14 02:41:08 +0800

    Modification

--- Week-3/Assignments/main.py
+++ Week-3/Assignments/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hi! Im Adrian"
+    return render_template('index.html')
 
 @app.route('/data', methods=['GET', 'POST'])
 def data(number=None):
